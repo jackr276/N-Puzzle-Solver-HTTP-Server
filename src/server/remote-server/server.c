949f9985f8a25531f3bacdc8d5c51f0d248dae55
jackr276 <jack_m_robbins@me.com>
2024-08-12 17:32:21 -0400

    initial creation of minimal server header file

--- src/server/remote-server/server.c
+++ src/server/remote-server/server.c
@@ -6,3 +6,4 @@
 #include "server.h"
 
 
+
--- src/server/remote-server/server.h
+++ src/server/remote-server/server.h
@@ -6,7 +6,33 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+//Give ourselves a data buffer
+#define BUFFER 20000
 
+//For our socket functionality
+#include <netinet/in.h>
+#include <sys/types.h>
 
+/**
+ * Define a struct for a server that contains all of the needed information 
+ * for transmission
+ */
+typedef struct {
+	u_int32_t domain;
+	u_int32_t port;	
+	u_int32_t service;
+	u_int32_t protocol;
+	u_int32_t backlog;
+	u_int64_t interface;	
+
+	u_int32_t socket;
+	struct sockaddr_in socket_addr;
+	
+} Server;
+
+//A constructor for our server
+Server create_server(u_int32_t domain, u_int32_t port, u_int32_t service, u_int32_t backlog, u_int64_t interface);
+//A function that serves to start everything up
+void launch(Server* server);
 
 #endif
