52bfc1bc22a0fb11771d8ee92c4bcced28e242ba
jackr276 <jack_m_robbins@me.com>
2024-08-13 10:43:23 -0400

    Update response_builder.h

--- src/server/response_builder/response_builder.h
+++ src/server/response_builder/response_builder.h
@@ -7,10 +7,14 @@
 #define RESPONSE_BUILDER_H
 
 struct response{
-
+	//The response mainly contains the HTML code that we want to serve up
+	char* html;
 };
 
-
+/**
+ * Server up the initial landing page response
+ */
+struct response initial_landing_response();
 
 #endif
 
