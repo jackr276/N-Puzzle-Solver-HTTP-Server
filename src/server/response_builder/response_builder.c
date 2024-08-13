87873568c7cf89493cdbb1b92e8ba2fa50b1390d
jackr276 <jack_m_robbins@me.com>
2024-08-13 10:37:40 -0400

    begin implementation of the response builder

---
+++ src/server/response_builder/response_builder.c
@@ -0,0 +1,10 @@
+/**
+ * Author: Jack Robbins
+ * This implementation file implements the response functionality described in the header file
+ * of the same name
+ */
+
+#include "response_builder.h"
+
+
+
---
+++ src/server/response_builder/response_builder.h
@@ -0,0 +1,16 @@
+/**
+ * Author: Jack Robbins
+ * This header file contains the prototypes of functions used in crafting the HTML response for our server
+ */
+
+#ifndef RESPONSE_BUILDER_H
+#define RESPONSE_BUILDER_H
+
+struct response{
+
+};
+
+
+
+#endif
+
