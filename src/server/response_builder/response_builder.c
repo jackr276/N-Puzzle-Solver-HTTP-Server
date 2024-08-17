d49dd4f4d9596dd79043569e1885e360cd1792f7
jackr276 <jack_m_robbins@me.com>
2024-08-17 16:06:16 -0400

    begin overhaul of the response builder subsytem

--- src/server/response_builder/response_builder.c
+++ src/server/response_builder/response_builder.c
@@ -5,6 +5,24 @@
  */
 
 #include "response_builder.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * Construct the HTML necessary for the grid display that we need on the screen
+ */
+static char* construct_grid_display(const int N, struct state* state_ptr){
+	char* grid_display = (char*)malloc(2000);
+
+	//First print in the start of the grid
+	sprintf(grid_display, "<div class = \"wrapper\">");
+
+
+	return grid_display;
+}
+
+
 
 /**
  * Construct the response that the user initially gets on the landing page
@@ -13,15 +31,18 @@ struct response initial_landing_response(){
 	//Stack allocate our response
 	struct response r;
 
+	//Allocate the space that we need for our initial html
+	r.html = (char*)malloc(RESPONSE_SIZE);
+
 	//Populate the initial HTML
-	r.html = "HTTP/1.1 200 OK\r\n"
-			 "Connection: keep-alive\r\n"
-             "Content-Type: text/html; charset=UTF-8\r\n"
-			 "Keep-Alive: timeout=15, max=1000\r\n\r\n"
-             "<!DOCTYPE html>\r\n"
-             "<html>\r\n"
-             "<head>\r\n"
-             "<title>N Puzzle Solver</title>\r\n"
+	sprintf(r.html, "HTTP/1.1 200 OK\r\n"
+			 				   "Connection: keep-alive\r\n"
+             				   "Content-Type: text/html; charset=UTF-8\r\n"
+			 				   "Keep-Alive: timeout=15, max=1000\r\n\r\n"
+           				 	   "<!DOCTYPE html>\r\n"
+             				   "<html>\r\n"
+             				   "<head>\r\n"
+             				   "<title>N Puzzle Solver</title>\r\n"
              "</head>\r\n"
              "<body>\r\n"
 			 "<h1>N Puzzle Solver</h1>\r\n"
@@ -33,7 +54,22 @@ struct response initial_landing_response(){
 		     "<input type=\"submit\" value=\"Generate Start Config\">\r\n"
 			 "</form>\r\n"
              "</body>\r\n"
-             "</html>\r\n\r\n";
+             "</html>\r\n\r\n");
+
+	//Save the type in here for later
+	r.type = RSP_INITIAL;
 
 	return r;
 }
+
+
+struct response initial_config_response(const int N, struct state* state_ptr){ 
+	//Stack allocate our response
+	struct response r;
+
+
+	return r;
+}
+
+
+
--- src/server/response_builder/response_builder.h
+++ src/server/response_builder/response_builder.h
@@ -6,7 +6,20 @@
 #ifndef RESPONSE_BUILDER_H
 #define RESPONSE_BUILDER_H
 
+#define RESPONSE_SIZE 20000
+
 #include <string.h>
+#include "../npuzzle/puzzle/puzzle.h"
+
+/**
+ * The type of HTML response given
+ */
+typedef enum {
+	RSP_INITIAL,
+	RSP_INITIAL_CONF,
+	//TODO we definitely need more in here
+
+} response_type;
 
 /**
  * A response struct that holds the various things needed with our html string
@@ -14,15 +27,22 @@
 struct response{
 	//The response mainly contains the HTML code that we want to serve up
 	char* html;
+	response_type type;
 };
 
-
 /**
- * Server up the initial landing page response
+ * Serve up the initial landing page response
  */
 struct response initial_landing_response();
 
+/**
+ * Serve up the response that has the initial grid displayed
+ */
+struct response initial_config_response(const int, struct state*);
 
-
+/**
+ * Teardown any dynamically allocated memory components in the response
+ */
+void teardown_response(struct response);
 #endif
 
